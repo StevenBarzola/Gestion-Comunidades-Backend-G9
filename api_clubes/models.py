638dd89a0f6c98ef30647ce71cd703c5a041c9d0
StevenBarzola <150624594+StevenBarzola@users.noreply.github.com>
2026-08-10 20:54:42 +0000

    Conexion y Configuracion BD

---
+++ api_clubes/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# ==========================================
+# 1. PARTE DE STEVEN: Membresías y Directorio
+# ==========================================
+# Aquí irán los modelos: Club, RolUsuario, etc.
+
+
+# ==========================================
+# 2. PARTE DE JULIO: Inventario
+# ==========================================
+# Aquí irán los modelos: Item, Categoria, etc. (Debe relacionarse con Club)
+
+
+# ==========================================
+# 3. PARTE DE ISAAC: Asambleas y Votaciones
+# ==========================================
+# Aquí irán los modelos: Asamblea, OpcionVoto, Voto, etc.
